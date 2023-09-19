3af8aec28929c66749a083e4c4518b868084c9d2
Mario Nageh <marionageh7@gmail.com>
2023-09-19 09:34:42 +0300

    Add Mysql Setup For Dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 -- Then We Create The User
 CREATE USER IF NOT EXISTS `hbnb_dev`@`localhost` IDENTIFIED BY 'hbnb_dev_pwd';
 -- Then We Grant All Privileges To The User
-GRANT ALL PRIVILEGES ON *.* TO `hbnb_dev`@`localhost`;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO `hbnb_dev`@`localhost`;
 -- Grant SELECT To The User on 'performance_schema' DB
 GRANT SELECT ON performance_schema.* TO `hbnb_dev`@`localhost`;
