e44fe35152aa9956cb6596543d8d2e3f75bf4350
Mario Nageh <marionageh7@gmail.com>
2023-09-20 16:54:25 +0300

    Fix Places Module

--- models/review.py
+++ models/review.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel, ExtendedBase, Base
-
+from models.base_model import BaseModel, Base, ExtendedBase
+from sqlalchemy import Column, String, DateTime,ForeignKey
 
 class Review(BaseModel):
     """ Review classto store review information """
+    __tablename__ = 'reviews'
     place_id = ""
     user_id = ""
     text = ""
