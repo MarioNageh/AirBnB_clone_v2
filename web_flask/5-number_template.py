895cdabaa3c08c8ecda2d62a3359c6620718e663
Mario Nageh <marionageh7@gmail.com>
2023-10-23 03:51:38 +0300

    Add Templates and Pass Variable to it

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""""
+from flask import Flask,render_template
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """handling varible routes"""
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """handling varible routes"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_number(n):
+    """handling numbers only"""
+    return f"{escape(n)} is a number"
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    """handling number and pass it to html template"""
+    return render_template("5-number.html", number=n)
+
+
+if __name__ == '__main__':
+    """Listening on"""
+    app.run(port=5000, host='0.0.0.0')
