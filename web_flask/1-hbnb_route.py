395ea77b850b0c88da89085d6e9f7e3a628a0319
Mario Nageh <marionageh7@gmail.com>
2023-10-23 03:15:39 +0300

    fix route hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,7 +11,7 @@ def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnh', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays HBNB"""
     return "HBNB"
