f0844c44574888462d884804678e4629c5e16064
Mario Nageh <marionageh7@gmail.com>
2023-10-23 03:11:03 +0300

    Make Files Executable

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from flask import Flask
-
 """This module starts a Flask web application"""""
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -13,4 +12,5 @@ def hello():
 
 
 if __name__ == '__main__':
+    """Listening on"""
     app.run(port=5000, host='0.0.0.0')
